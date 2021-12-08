7b7093c505cc6e9fee759e1dd50be0d9d9ef830b
BarbaraRocha <barbaraluizasr@gmail.com>
2021-12-08 16:43:12 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,10 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
+const feedbackMsg = () => 'Eita pessoa boa!';
+const createStudent = (nome) => ({
+  name: nome,
+  feedback: feedbackMsg,
+});
 
 module.exports = createStudent;
